9768b7bab145b92e62290dbff6af6a7e9ce38098
sdao2 <sdao2@illinois.edu>
2016-05-04 20:45:46 -0500

    Sequential code

---
+++ sequential/sequentialSort.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <stdlib.h>
+#include <sys/time.h>
+
+double lastTime;
+
+double getTime() {
+  timeval now;
+  gettimeofday(&now, NULL);
+  return ((double) now.tv_sec) + ((double) now.tv_usec)/1000000.;
+}
+void start () {
+  lastTime = getTime();
+} 
+
+double stop () {
+  double d = (getTime()-lastTime);
+  return d;
+} 
+
+int main(int argc, char * argv[])
+{
+	FILE * fp;
+	FILE * writer;
+
+	if(argc < 2)
+	{
+		std::cout << "Please provide file to sort on. ( ./externalMergeSort example.txt )\n";
+		return 0;
+	}
+
+	fp = fopen(argv[1], "r+");
+
+	if( fp == NULL ) 
+   {
+      perror ("Error opening file");
+      return(-1);
+   }
+
+   fseek(fp, 0, SEEK_END);
+   int len = ftell(fp); //length of file in bytes
+   fseek(fp, 0, SEEK_SET);
+
+   std::vector <char> buffer(len);
+
+   std::cout << len << " " << std::endl;
+
+   int x;
+   int iter = 0;
+    
+   fread((void *)&buffer[0], sizeof(char), len , fp);
+
+   start();
+
+   std::sort (buffer.begin(), buffer.end());
+   
+   std::cout << "Time = " << stop() << std::endl;
+
+   writer = fopen("temp.txt", "w");
+   fwrite((void *)&buffer[0], sizeof(char), len, writer);
+   fclose(fp);
+   fclose(writer);
+
+
+}
